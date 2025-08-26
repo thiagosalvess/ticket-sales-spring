ebe0621c236e33535767f80388578c72f5f9f796
Thiago Alves <thiago.s.alves12@gmail.com>
2025-08-26 10:12:56 -0300

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/thiagosalvess/ticketsales/TicketsalesApplication.java
@@ -0,0 +1,13 @@
+package com.thiagosalvess.ticketsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketsalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiagosalvess/ticketsales/TicketsalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagosalvess.ticketsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
